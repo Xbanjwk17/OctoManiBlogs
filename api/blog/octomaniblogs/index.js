a9f30cbf57f642b0798eb150c7ab776f9494a614
Xbanjwk <123902772+Xbanjwk17@users.noreply.github.com>
2023-02-08 17:20:06 -0300

    Update index.js

--- api/blog/octomaniblogs/index.js
+++ api/blog/octomaniblogs/index.js
@@ -1,4 +1,7 @@
 var data = {
    blogname: "octomaniblogs",
-   blogdescription: "Hello World!"
+   blogdescription: "Hello World!",
+   blogpostdata: [
+       {posttitle: "Welcome!", postcontent: "Welcome for it! Members are official.", id: 1, createddate: new Date("02/08/2023"), updateddate: new Date("02/08/2023")}
+   ]
 }
